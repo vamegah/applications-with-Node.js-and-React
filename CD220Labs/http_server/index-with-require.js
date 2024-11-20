b1fcab21251e813ba2c4bdb5980e2f265baac8a8
vamegah <makvim@yahoo.com>
2024-11-20 12:27:37 -0500

    Added greetings based on current time

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -7,8 +7,22 @@ const today = require('./today');
 // Define the request listener function
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
-    // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+    let dateVal = today.getDate(); // Get the current date from the 'today' module
+
+    // Determine the appropriate greeting based on the current time
+    let greeting = "It is still not morning";
+    if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+        greeting = "Good morning!";
+    } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+        greeting = "Good afternoon!";
+    } else if (dateVal.getHours() >= 18 && dateVal.getHours() < 21) {
+        greeting = "Good evening!";
+    } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
+        greeting = "Good night!";
+    }
+
+    // Send the response with the appropriate greeting
+    res.end(`Hello, ${greeting}`);
 };
 
 // Define the port number
